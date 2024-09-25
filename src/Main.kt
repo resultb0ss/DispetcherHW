d6e0446ef61c1306c7a940d5539f505451f64884
Admin <reteamboss@gmail.com>
2024-09-25 10:20:57 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,64 @@
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+suspend fun main() = coroutineScope {
+    val personList: MutableList<Person> = mutableListOf()
+    val myPerson = PersonManager(personList)
+    val resultList: MutableMap<Person, Int> = mutableMapOf()
+
+    println("Программа работы с базой данных сотрудников")
+    delay(1000L)
+    while (true){
+        println("Добавить сотрудника \n 1.Да \n 2.Нет")
+        val inputMain = readln()
+        when(inputMain){
+            "1" -> {
+                val myPerson = PersonManager(personList)
+                val person = myPerson.addPerson()
+                launch {person.addPassword(resultList)}
+                println("Добавить сотрудника: \n1.Да \n2.Прочитать базу данных")
+                val inputSecond = readln()
+                when(inputSecond){
+                    "1" -> continue
+                    "2" -> {
+                        readDataPersonList(resultList)
+                        continue
+                    }
+                }
+            }
+            "2" -> {
+                println("Завершение работы")
+
+                break
+
+
+            }
+        }
+    }
+
+
+
+
 }
+
+suspend infix fun Person.addPassword(mapPerson: MutableMap<Person,Int>){
+    val pass = (100000..999999).random()
+    mapPerson[this] = pass
+    delay(500L)
+    println("Пароль успешно создан")
+}
+
+suspend fun readDataPersonList(mapPerson: MutableMap<Person, Int>){
+    for (i in mapPerson){
+        println("Сотрудник ${i.key.name}; пароль: ${i.value}")
+        delay(1000L)
+    }
+}
+
+
+
+
+
+data class Person(val name: String,val salary: Int)
--- src/PersonManager.kt
+++ src/PersonManager.kt
@@ -1,2 +1,15 @@
-class PersonManager {
+class PersonManager(var personList: MutableList<Person>) {
+
+
+    fun addPerson():Person{
+        println("Введите имя сотрудника")
+        val inputName = readln()
+        println("Введите зарплату сотрудника")
+        val inputSalary = readln()
+        val person = Person(inputName,inputSalary.toInt())
+        personList.add(person)
+
+        return person
+    }
+
 }
